d98f409b237dbe70762992db5f836ec1f10dbb9b
Ming Lee <ming.lee@hotmail.com>
2017-09-06 15:29:13 +0100

    Add api folder.

---
+++ api/models/todo.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Todo = sequelize.define('Todo', {
+    title: {
+      DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Todo.hasMany(models.TodoItem, {
+          foreignKey: 'todoId',
+          as: 'todoItems',
+        });
+      }
+    }
+  });
+  return Todo;
+};

c78b489cb394ff13543555c6dfa60cc2e45e9ec7
Ming Lee <ming.lee@hotmail.com>
2017-09-07 16:05:59 +0100

    Add todo list get list and create route.

---
+++ api/routes/todos.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var todosController = require('../controllers').todos;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'To do list' });
+});
+
+router.post('/create', todosController.create);
+router.get('/list', todosController.list);
+
+module.exports = router;
